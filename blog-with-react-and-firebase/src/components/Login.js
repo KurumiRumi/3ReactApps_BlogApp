b97877e61ee0766717d73d6395ccf1a526ae8f9e
KurumiRumi <flora.fluorite.caf2@gmail.com>
2024-02-08 12:38:50 +0900

    32. 認証状態を保存する変数をローカルストレージに保存してみよう

--- blog-with-react-and-firebase/src/components/Login.js
+++ blog-with-react-and-firebase/src/components/Login.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  return <div>Login</div>;
+import { auth, provider } from "../firebase";
+
+const Login = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+
+  const loginInWithGoogle = () => {
+    // Googleでログイン
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      navigate("/");
+    });
+  };
+
+  return (
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginInWithGoogle}>Googleでログイン</button>
+    </div>
+  );
 };
 
 export default Login;

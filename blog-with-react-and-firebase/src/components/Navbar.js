e3510cb6bc5466e5d73ed3a4f95e57bce78eee38
KurumiRumi <flora.fluorite.caf2@gmail.com>
2024-02-08 12:50:19 +0900

    34. ログインとログアウトボタンを切替えてみよう

--- blog-with-react-and-firebase/src/components/Navbar.js
+++ blog-with-react-and-firebase/src/components/Navbar.js
@@ -8,7 +8,7 @@ import {
   faArrowRightToBracket,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Navbar = () => {
+const Navbar = ({ isAuth }) => {
   return (
     <nav>
       <Link to="/">
@@ -21,10 +21,17 @@ const Navbar = () => {
         記事投稿
       </Link>
 
-      <Link to="/login">
-        <FontAwesomeIcon icon={faArrowRightToBracket} />
-        ログイン
-      </Link>
+      {!isAuth ? (
+        <Link to="/login">
+          <FontAwesomeIcon icon={faArrowRightToBracket} />
+          ログイン
+        </Link>
+      ) : (
+        <Link to="/logout">
+          <FontAwesomeIcon icon={faArrowRightToBracket} />
+          ログアウト
+        </Link>
+      )}
     </nav>
   );
 };

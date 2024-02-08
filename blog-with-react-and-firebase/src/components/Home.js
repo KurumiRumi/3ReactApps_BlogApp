d0443ff4bfb7c02ed3c5751e736ddc00cec15ece
KurumiRumi <flora.fluorite.caf2@gmail.com>
2024-02-08 13:28:13 +0900

    40. Homeコンポーネントを作成しよう

--- blog-with-react-and-firebase/src/components/Home.js
+++ blog-with-react-and-firebase/src/components/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+      </div>
+      <div className="postTextContainer">
+        今はReactの勉強中です。今はReactの勉強中です。今はReactの勉強中です。今はReactの勉強中です。今はReactの勉強中です。
+      </div>
+      <div className="nameAndDeleteButton">
+        <h3>@ShinCode</h3>
+        <button>削除</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
